b57ac2194acd6491aef3aa3da1ec02f7ae595162
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-06-16 18:50:33 +0100

    feat: Implemented Generics in TS

--- src/app.ts
+++ src/app.ts
@@ -26,8 +26,41 @@ form.addEventListener('submit', (e: Event) => {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
     }
 
-    list.render(doc, type.value, 'end');
-
-    
+    list.render(doc, type.value, 'end');  
 });
+
+// GENERICS
+
+const addUID = <T extends {name: string}>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+
+console.log(docOne.age);
+
+// with interfaces
+interface Resource<T> {
+    uid: number;
+    resourceName: string;
+    data: T;
+}
+
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+}
+
+const docFour: Resource<string[]> = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk', 'toilet roll']
+}
+
+console.log(docThree, docFour)
+
+
 
